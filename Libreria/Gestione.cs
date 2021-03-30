c6e34744f232b529ae8859c43a2a1a9815be9f33
michiburro14@gmail.com <michiburro14@gmail.com>
2021-03-30 20:54:58 +0200

    Add project files.

---
+++ Libreria/Gestione.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Libreria
+{
+    public class Gestione
+    {
+        public static string RiconosciTriangoli(double lato1, double lato2, double Lato3)
+        {
+            string risultato = "";
+            if (lato1 > 0 && lato2 > 0 && Lato3 > 0)
+            {
+                if (lato1 == lato2 && lato1 == Lato3)
+                {
+                    risultato = "Equilatero";
+                }
+                else if (lato1 != lato2 && lato2 != Lato3 && Lato3 != lato1)
+                {
+                    risultato = "Scaleno";
+                }
+                else
+                {
+                    risultato = "Isoscele";
+                }
+            }
+            else
+                risultato = "IMPOSSIBILE";
+            return risultato;          
+        }
+        public static double Perimetro(double lato1, double lato2, double lato3)
+        {
+            return lato1 + lato2 + lato3;
+        }
+
+        public static double Area(double lato1, double lato2, double lato3)
+        {
+            double semiperimetro = (Perimetro(lato1, lato2, lato3)) / 2;
+            return Math.Round((Math.Sqrt(semiperimetro)) * (semiperimetro - lato1) * (semiperimetro - lato2) * (semiperimetro - lato3),2);
+        }
+    }
+}
---
+++ Triangoli/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Libreria;
+
+namespace Triangoli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_Lato1.Text != " " && txt_Lato2.Text != " " && txt_Lato3.Text != " ")
+            {
+                try
+                {
+                    double lato1 = double.Parse(txt_Lato1.Text);
+                    double lato2 = double.Parse(txt_Lato2.Text);
+                    double lato3 = double.Parse(txt_Lato3.Text);
+
+                    lbl_TipoTriangolo.Content = Gestione.RiconosciTriangoli(lato1, lato2, lato3);
+                    lbl_Area.Content = Gestione.Area(lato1, lato2, lato3);
+                    lbl_Perimetro.Content = Gestione.Perimetro(lato1, lato2, lato3);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Valori non validi!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Non hai inserito valori!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
